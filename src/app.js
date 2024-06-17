0078516df3d6b47230f215591ecd96316522a2d0
Federico Gabriel Gutierrez <gutierrezfedericog@gmail.com>
2024-06-16 21:47:48 -0300

    PreEntrega2-Gutirrez

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ const productManager = new ProductManager('./src/data/products.json');
 const io = new Server(httpServer); 
 
 io.on("connection", async (socket) => {
-  console.log("Usuario conectato correctamente"); 
+  console.log("Usuario conectado"); 
 
   socket.emit("products", await productManager.getProducts());
 
--- src/routes/view.routes.js
+++ src/routes/view.routes.js
@@ -1,9 +1,27 @@
 import { Router } from 'express';
+import ProductManager from "../utils/productManager.js";
+const productManager = new ProductManager("./src/data/products.json");
 
 const router = Router();
 
 router.get('/realtimeproducts', async(req, res) => {
     res.render('realTimeProducts');
 })
+
+router.get("/home", async (req, res) => {
+    try {
+        const limit = req.query.limit;
+        let productos = await productManager.getProducts();
+        if (limit) {
+            productos = productos.slice(0, limit);
+        }
+        res.render('home', { productos });
+    } catch (error) {
+        console.error("Error al obtener productos", error);
+        res.status(500).json({
+            error: "Error interno del servidor"
+        });
+    }
+});
 
 export default router;
